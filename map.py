35d3ab78335f93aed9db74afb622a32a4b5fea95
Faerbit <faerbit@gmail.com>
2015-10-18 13:34:03 +0200

    Added start and target to map.

--- map.py
+++ map.py
@@ -13,6 +13,8 @@ class Map():
 
     def __init__(self):
         self.map = list()
+        self.start = Point(0, 0)
+        self.target = Point(0, 0)
 
     def __repr__(self):
         return str(self.map)
@@ -25,15 +27,20 @@ class Map():
         """ Loads a map from a file. """
         for line in open(filename):
             split_line = line.split()
-            if split_line[0].upper() == "W":
+            if split_line[0].lower() == "w":
                 p = Point(int(split_line[1]), int(split_line[2]))
                 q = Point(int(split_line[3]), int(split_line[4]))
                 self.add_line(p, q)
+            elif split_line[0].lower() == "s":
+                    self.start = Point(int(split_line[1]), int(split_line[2]))
+            elif split_line[0].lower() == "t":
+                    self.target = Point(int(split_line[1]), int(split_line[2]))
             else:
                 raise Exception("Unsupported character at the beginning of line: " + line)
 
 
     def save_svg(self, filename, offset=5):
+        """ Saves a graphical representation of the map to a SVG file. """
         svg = svgwrite.Drawing(filename)
         # get dimensions
         max_x = 0
@@ -48,6 +55,10 @@ class Map():
             p = (line[0][0] + offset, line[0][1] + offset)
             q = (line[1][0] + offset, line[1][1] + offset)
             walls.add(svg.line(p, q))
+        start_x, start_y = self.start
+        svg.add(svg.circle((start_x + offset, start_y + offset), fill="green"))
+        target_x, target_y = self.target
+        svg.add(svg.circle((target_x + offset, target_y + offset), fill="blue"))
         svg.save()
 
 def main():
